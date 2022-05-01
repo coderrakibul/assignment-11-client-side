4b18d8e21bccd93a63c053f55576cb31df4fb467
coderrakibul <coder.rakibul@gmail.com>
2022-05-01 16:52:46 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear()
     return (
         <div className='bg-dark p-5'>
-            <p className='text-light p-2'>Copyright &copy; All Rights Are Reserved</p>
+            <p className='text-light p-2'>Copyright &copy; All Rights Are Reserved : {year}</p>
+
+
         </div>
     );
 };
