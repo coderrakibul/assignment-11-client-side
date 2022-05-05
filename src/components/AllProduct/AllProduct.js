acb1bbbcbf2c494992beb1f3c28b001fdf6a1d93
coderrakibul <coder.rakibul@gmail.com>
2022-05-05 09:57:23 +0600

    homepage updated

--- src/components/AllProduct/AllProduct.js
+++ src/components/AllProduct/AllProduct.js
@@ -1,25 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import Loading from '../Loading/Loading';
 import SingleProduct from '../SingleProduct/SingleProduct';
 import './AllProduct.css';
 
 const AllProducts = () => {
 
     const [products, setProducts] = useState([]);
-    let loading;
 
     useEffect(() => {
         fetch("http://localhost:5000/product")
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
-
-    if (loading) {
-        if (loading) {
-            return <Loading></Loading>
-        }
-    };
-
 
     return (
 
