424db4c36e7ec1e6ebff62619450ada6bdbfe937
coderrakibul <coder.rakibul@gmail.com>
2022-05-06 14:57:33 +0600

    signout method implemented

--- src/components/Header/Header .js
+++ src/components/Header/Header .js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
+
+
+
 
 const Header  = () => {
+  const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
     return (
       <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
@@ -13,6 +25,14 @@ const Header  = () => {
       <Nav.Link as={Link} to="/home">Home</Nav.Link>
       
      
+      {
+
+user?
+<Nav.Link onClick={handleSignOut} >Signout</Nav.Link> 
+:
+<Nav.Link as={Link} to="/login">Login</Nav.Link>
+
+}
      
     </Nav>
     
@@ -22,4 +42,4 @@ const Header  = () => {
     );
 };
 
-export default Header ;
+export default Header;
