5426442626dc57ba315f36910d9e6d8808e2de84
coderrakibul <coder.rakibul@gmail.com>
2022-05-07 23:54:02 +0600

    description edited

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -16,14 +16,14 @@ const Inventory = () => {
 
 
     return (
-        <div className='container'>
-            <h2 className='text-center mt-5'>About This Product</h2>
+        <div className='container mt-5 mb-5'>
+            <h2 className='text-center'>About This Product</h2>
             <img className='w-50 mx-auto d-block' src={product.image} alt="" />
-            <h3>{product.name}</h3>
-            <h3>{product.price}</h3>
-            <h3>{product.quantity}</h3>
-            <h3>{product.supplier}</h3>
-            <h5>{product.description}</h5>
+            <h3>Name: {product.name}</h3>
+            <h3>Price: ${product.price}</h3>
+            <h3>Available: {product.quantity}</h3>
+            <h3>Supplier: {product.supplier}</h3>
+            <h5> <h3 className='d-inline-block'>Description:</h3> {product.description}</h5>
         </div>
     );
 };
