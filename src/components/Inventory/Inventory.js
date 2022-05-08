3dd13f2c9c5a76b4bd9393d7422554f99fad9b7a
coderrakibul <coder.rakibul@gmail.com>
2022-05-08 17:49:58 +0600

    delete button added

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -13,43 +13,60 @@ const Inventory = () => {
 
     useEffect(() => {
         const url = `https://quiet-dawn-78359.herokuapp.com/product/${productId}`;
-        console.log(url);
         fetch(url)
             .then(res => res.json())
             .then(data => setProduct(data));
     }, [productId]);
 
+    const handleDelete = () =>{
+        const proceed = window.confirm('are you sure?');
+        if(proceed){
+            const url = `https://quiet-dawn-78359.herokuapp.com/product/${productId}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+            
+        }
+    }
+
 
     return (
-       <div>
-           <div className='mt-3 mb-5 d-flex justify-content-center'>
-           <Link className='btn btn-primary' as={Link} to="/additem">Add New Product</Link>
-           </div>
+        <div>
+            <div className='mt-3 mb-5 d-flex justify-content-center'>
+                <Link className='btn btn-primary' as={Link} to="/additem">Add New Product</Link>
+            </div>
 
             <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>Image</th>
-                    <th>Product Name</th>
-                    <th>Email</th>
-                    <th>Available</th>
-                    <th>Price</th>
-                    <th>Supplier</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td className='img-width'><img className='w-50' src={product.image} alt="" /></td>
-                    <td className='name-width'>{product.name}</td>
-                    <td className='email-width'>{user.email}</td>
-                    <td className='quantity-width'>{product.quantity}</td>
-                    <td className='price-width'>${product.price}</td>
-                    <td className='supplier-width'>{product.supplier}</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Product Name</th>
+                        <th>Email</th>
+                        <th>Available</th>
+                        <th>Price</th>
+                        <th>Supplier</th>
 
-            </tbody>
-        </Table>
-       </div>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='img-width'><img className='w-50' src={product.image} alt="" /></td>
+                        <td className='name-width'>{product.name}</td>
+                        <td className='email-width'>{user.email}</td>
+                        <td className='quantity-width'>{product.quantity}</td>
+                        <td className='price-width'>${product.price}</td>
+                        <td className='supplier-width'>{product.supplier}</td>
+                        <td className='button-width'><button onClick={handleDelete} className='btn btn-danger'>Delete</button></td>
+
+                    </tr>
+
+                </tbody>
+            </Table>
+        </div>
     );
 };
 
